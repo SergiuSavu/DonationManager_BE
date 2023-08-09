a6231f0593088e2a824c95c4e0cfc1becb573670
Sergiu Savu <Sergiu.Savu@msg.group>
2023-08-09 11:05:54 +0300

    Am modificat entitile Role, User si am adaugat PermissionEnum care tine loc de tabel pentru permisiuni

---
+++ src/main/java/de/msg/javatraining/donationmanager/persistence/model/PermissionEnum.java
@@ -0,0 +1,42 @@
+package de.msg.javatraining.donationmanager.persistence.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public enum PermissionEnum {
+    PERMISSION_MANAGEMENT(1,"PERMISSION_MANAGEMENT"),
+    USER_MANAGEMENT(2, "USER_MANAGEMENT"),
+    CAMP_MANAGEMENT(3,"CAMP_MANAGEMENT"),
+    BENEF_MANAGEMENT(4, "BENEF_MANAGEMENT"),
+    DONATION_MANAGEMENT(5, "DONATION_MANAGEMENT"),
+    DONATION_APPROVE(6, "DONATION_APPROVE"),
+    DONATION_REPORTING(7, "DONATION_REPORTING"),
+    CAMP_REPORTING(8, "CAMP_REPORTING"),
+    CAMP_IMPORT(9, "CAMP_IMPORT"),
+    CAMP_REPORT_RESTRICTED(10, "CAMP_REPORT_RESTRICTED");
+
+    private int id;
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    //INSERT INTO ROLE_PERMISSION (idRole, idPermission) values (2,PermissionEnum.PERMISSION_MANAGEMENT.getId())
+}
+
