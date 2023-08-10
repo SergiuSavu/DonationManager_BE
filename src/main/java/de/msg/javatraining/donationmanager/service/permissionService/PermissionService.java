2f8c14ba0b0c11cd93468f3c78c2517794545660
Timea Gal <timea.gal@msg.group>
2023-08-10 15:39:55 +0200

    adaugare, stergere, editare drepturi + teste + pom.xml

--- src/main/java/de/msg/javatraining/donationmanager/service/permissionService/PermissionService.java
+++ src/main/java/de/msg/javatraining/donationmanager/service/permissionService/PermissionService.java
@@ -1,5 +1,6 @@
 package de.msg.javatraining.donationmanager.service.permissionService;
 
+import de.msg.javatraining.donationmanager.persistence.model.ERole;
 import de.msg.javatraining.donationmanager.persistence.model.PermissionEnum;
 import de.msg.javatraining.donationmanager.persistence.model.Role;
 import de.msg.javatraining.donationmanager.persistence.model.User;
@@ -7,7 +8,11 @@ import de.msg.javatraining.donationmanager.persistence.repository.RoleRepository
 import de.msg.javatraining.donationmanager.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class PermissionService {
@@ -18,62 +23,118 @@ public class PermissionService {
     @Autowired
     private RoleRepository roleRepository;
 
-    public boolean addPermissionToUser(Long userId, PermissionEnum permission) {
-        Optional<User> optionalUser = userRepository.findById(userId);
-
-        if (optionalUser.isPresent()) {
-            User user = optionalUser.get();
-
-            // Check if the user has PERMISSION_MANAGEMENT permission
-            if (!hasPermission(user, PermissionEnum.PERMISSION_MANAGEMENT)) {
-                return false; // User doesn't have the required permission
-            }
-
-            // Find the role containing the permission
-            Optional<Role> permissionRole = findRoleWithPermission(permission);
-
-            if (permissionRole.isPresent()) {
-                user.getRoles().add(permissionRole.get());
-                userRepository.save(user);
-                return true; // Permission added successfully
-            }
+    public boolean addPermissionToUser(Long userIdADMIN, Long userIdTARGET, PermissionEnum permissionToAdd) {
+        if (permissionToAdd == null) {
+            return false; // Invalid input
         }
 
+        Optional<User> userADMIN = userRepository.findById(userIdADMIN);
+        Optional<User> userTARGET = userRepository.findById(userIdTARGET);
+
+        if (!userADMIN.isPresent() || !userTARGET.isPresent()) {
+            return false; // User not found
+        }
+
+        if (userADMIN != null && userTARGET != null) {
+            PermissionEnum adminPermissionToCheck = PermissionEnum.PERMISSION_MANAGEMENT;
+
+            for (Role adminRole : userADMIN.get().getRoles()) {
+                if (adminRole.getPermissions().contains(adminPermissionToCheck)) {
+
+                    for (Role targetRole : userTARGET.get().getRoles()) {
+                        if (targetRole.getName() == ERole.ROLE_ADM
+                        && !targetRole.getPermissions().contains(permissionToAdd) &&
+                                (permissionToAdd == PermissionEnum.PERMISSION_MANAGEMENT || permissionToAdd == PermissionEnum.USER_MANAGEMENT))
+                        {targetRole.getPermissions().add(permissionToAdd);
+                            roleRepository.save(targetRole);
+                            return true; // Permission added successfully
+                        }
+                        else if (targetRole.getName() == ERole.ROLE_MGN
+                                && !targetRole.getPermissions().contains(permissionToAdd) &&
+                                (permissionToAdd == PermissionEnum.CAMP_MANAGEMENT || permissionToAdd == PermissionEnum.BENEF_MANAGEMENT
+                                || permissionToAdd == PermissionEnum.DONATION_MANAGEMENT || permissionToAdd == PermissionEnum.DONATION_APPROVE
+                                || permissionToAdd == PermissionEnum.DONATION_REPORTING || permissionToAdd == PermissionEnum.CAMP_REPORTING
+                                || permissionToAdd == PermissionEnum.CAMP_IMPORT))
+                        {
+                            targetRole.getPermissions().add(permissionToAdd);
+                            roleRepository.save(targetRole);
+                            return true; // Permission added successfully
+                        }
+                         else if (targetRole.getName() == ERole.ROLE_CEN
+                                && !targetRole.getPermissions().contains(permissionToAdd) &&
+                                (permissionToAdd == PermissionEnum.BENEF_MANAGEMENT || permissionToAdd == PermissionEnum.DONATION_MANAGEMENT ||
+                                        permissionToAdd == PermissionEnum.DONATION_REPORTING || permissionToAdd == PermissionEnum.CAMP_REPORTING))
+                         {
+                             targetRole.getPermissions().add(permissionToAdd);
+                             roleRepository.save(targetRole);
+                             return true; // Permission added successfully
+                         }
+                         else if (targetRole.getName() == ERole.ROLE_REP && !targetRole.getPermissions().contains(permissionToAdd) &&
+                                permissionToAdd == PermissionEnum.CAMP_REPORT_RESTRICTED)
+                         {
+                             targetRole.getPermissions().add(permissionToAdd);
+                             roleRepository.save(targetRole);
+                             return true; // Permission added successfully
+                         }
+                    }
+
+                    }
+                }
+            }
         return false; // User not found or permission not available
     }
 
-    public boolean deletePermissionFromUser(Long userId, PermissionEnum permission) {
-        Optional<User> optionalUser = userRepository.findById(userId);
 
-        if (optionalUser.isPresent()) {
-            User user = optionalUser.get();
+    public boolean deletePermissionFromUser(Long userIdADMIN, Long userIdTARGET, PermissionEnum permissionToDelete) {
+        if (userIdADMIN == null || userIdTARGET == null || permissionToDelete == null) {
+            return false; // Invalid input
+        }
 
-            // Check if the user has PERMISSION_MANAGEMENT permission
-            if (!hasPermission(user, PermissionEnum.PERMISSION_MANAGEMENT)) {
-                return false; // User doesn't have the required permission
-            }
+        Optional<User> userADMIN = userRepository.findById(userIdADMIN);
+        Optional<User> userTARGET = userRepository.findById(userIdTARGET);
 
-            // Find the role containing the permission
-            Optional<Role> permissionRole = findRoleWithPermission(permission);
+        if (!userADMIN.isPresent() || !userTARGET.isPresent()) {
+            return false; // User not found
+        }
 
-            if (permissionRole.isPresent()) {
-                user.getRoles().remove(permissionRole.get());
-                userRepository.save(user);
-                return true; // Permission removed successfully
+        boolean i = false;
+        if (userADMIN != null && userTARGET != null) {
+            PermissionEnum adminPermissionToCheck = PermissionEnum.PERMISSION_MANAGEMENT;
+
+            for (Role adminRole : userADMIN.get().getRoles()) {
+                if (adminRole.getPermissions().contains(adminPermissionToCheck)) {
+                    for (Role targetRole : userTARGET.get().getRoles()) {
+                        if (targetRole.getPermissions().contains(permissionToDelete)) {
+                            targetRole.getPermissions().remove(permissionToDelete);
+                            roleRepository.save(targetRole);
+                            i = true; // Permission removed successfully
+                        }
+                    }
+                }
             }
         }
 
-        return false; // User not found or permission not available
+        return i; // User not found or permission not available
     }
 
-    private boolean hasPermission(User user, PermissionEnum permission) {
-        return user.getRoles().stream()
-                .anyMatch(role -> role.getPermissions().contains(permission));
+
+    public boolean hasPermission(User user, PermissionEnum permission) {
+        for (Role role : user.getRoles()) {
+            if (role.getPermissions().contains(permission)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    private Optional<Role> findRoleWithPermission(PermissionEnum permission) {
+    public Optional<Role> findRoleWithPermission(PermissionEnum permission) {
         return roleRepository.findAll().stream()
                 .filter(role -> role.getPermissions().contains(permission))
                 .findFirst();
+    }
+
+    public Set<Role> getRoles(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return optionalUser.map(User::getRoles).orElse(null);
     }
 }
