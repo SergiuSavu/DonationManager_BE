bfb238b2d826735c58853d6ceffd1216703693b1
Timea Gal <timea.gal@msg.group>
2023-08-11 15:53:49 +0200

    crud permission refactoring + tests

--- src/test/java/tests/PermissionControllerTest.java
+++ src/test/java/tests/PermissionControllerTest.java
@@ -2,18 +2,20 @@ package tests;
 
 import de.msg.javatraining.donationmanager.controller.permission.PermissionController;
 import de.msg.javatraining.donationmanager.persistence.model.PermissionEnum;
+import de.msg.javatraining.donationmanager.persistence.model.Role;
 import de.msg.javatraining.donationmanager.service.permissionService.PermissionService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class) // This annotation initializes the Mockito extensions for JUnit 5
 class PermissionControllerTest {
 
     @InjectMocks
@@ -22,40 +24,43 @@ class PermissionControllerTest {
     @Mock
     private PermissionService permissionService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
     @Test
     void testAddPermissionToUser_Success() {
-        when(permissionService.addPermissionToUser(anyLong(), anyLong(), any(PermissionEnum.class))).thenReturn(true);
+        when(permissionService.addPermissionToUser(anyLong(), any(Role.class), any(PermissionEnum.class)))
+                .thenReturn(PermissionEnum.CAMP_IMPORT);
 
-        ResponseEntity<Void> response = permissionController.addPermissionToUser(1L, 2L, PermissionEnum.CAMP_IMPORT);
+        ResponseEntity<Void> response = permissionController.addPermissionToUser(1L, new Role(), PermissionEnum.CAMP_IMPORT);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     void testAddPermissionToUser_Failure() {
-        when(permissionService.addPermissionToUser(anyLong(), anyLong(), any(PermissionEnum.class))).thenReturn(false);
+        when(permissionService.addPermissionToUser(anyLong(), any(Role.class), any(PermissionEnum.class)))
+                .thenReturn(null); // Simulating permission not granted
 
-        ResponseEntity<Void> response = permissionController.addPermissionToUser(1L, 2L, PermissionEnum.CAMP_IMPORT);
+        ResponseEntity<Void> response = permissionController.addPermissionToUser(1L, new Role(), PermissionEnum.CAMP_IMPORT);
         assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
     }
 
     @Test
     void testDeletePermissionFromUser_Success() {
-        when(permissionService.deletePermissionFromUser(anyLong(), anyLong(), any(PermissionEnum.class))).thenReturn(true);
+        when(permissionService.deletePermissionFromUser(anyLong(), any(Role.class), any(PermissionEnum.class)))
+                .thenReturn(PermissionEnum.BENEF_MANAGEMENT);
 
-        ResponseEntity<Void> response = permissionController.deletePermissionFromUser(1L, 2L, PermissionEnum.BENEF_MANAGEMENT);
+        ResponseEntity<Void> response = permissionController.deletePermissionFromUser(1L, new Role(), PermissionEnum.BENEF_MANAGEMENT);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     void testDeletePermissionFromUser_Failure() {
-        when(permissionService.deletePermissionFromUser(anyLong(), anyLong(), any(PermissionEnum.class))).thenReturn(false);
+        when(permissionService.deletePermissionFromUser(anyLong(), any(Role.class), any(PermissionEnum.class)))
+                .thenReturn(null); // Simulating permission not deleted
 
-        ResponseEntity<Void> response = permissionController.deletePermissionFromUser(1L, 2L, PermissionEnum.BENEF_MANAGEMENT);
+        ResponseEntity<Void> response = permissionController.deletePermissionFromUser(1L, new Role(), PermissionEnum.BENEF_MANAGEMENT);
         assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
     }
+
+    // Add more test cases as needed
+
+    // Remember to adjust return values and parameters based on your application's logic
 }
