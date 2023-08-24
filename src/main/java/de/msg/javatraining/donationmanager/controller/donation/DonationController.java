07819b73146320787a35cea1bf2022fbfb9019a8
Iordache Vlad <Vlad.Iordache@msg.group>
2023-08-24 12:14:23 +0300

    findDonorByCampaignId findDonationByCampaignId after merge

--- src/main/java/de/msg/javatraining/donationmanager/controller/donation/DonationController.java
+++ src/main/java/de/msg/javatraining/donationmanager/controller/donation/DonationController.java
@@ -1,9 +1,7 @@
 package de.msg.javatraining.donationmanager.controller.donation;
 
-import de.msg.javatraining.donationmanager.exceptions.donation.*;
-import de.msg.javatraining.donationmanager.exceptions.donator.DonatorRequirementsException;
-import de.msg.javatraining.donationmanager.exceptions.user.UserPermissionException;
 import de.msg.javatraining.donationmanager.persistence.donationModel.Donation;
+import de.msg.javatraining.donationmanager.exceptions.donation.DonationNotFoundException;
 import de.msg.javatraining.donationmanager.service.donationService.DonationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,83 +24,43 @@ public class DonationController {
     }
 
     @GetMapping("/{donationId}")
-    public ResponseEntity<?> getDonation(@PathVariable("donationId") Long donationId) {
-        try {
-            Donation donation = donationService.getDonationById(donationId);
-            return ResponseEntity.ok(donation);
-        } catch (DonationNotFoundException exception) {
-            return ResponseEntity.ok(exception.getMessage());
-        }
-        // return donationService.getDonationById(donationId);
-    }
-
-    @GetMapping("/{campaignId}")
-    public List<Donation> getDonatiosByCampaign(@PathVariable("campaignId") Long campaignId){
-
-            return   donationService.getDonationsByCampaignId(campaignId);
-
+    public ResponseEntity<?> getDonation(@PathVariable("donationId") Long donationId) throws DonationNotFoundException {
+        return donationService.getDonationById(donationId);
     }
 
     @PostMapping("/{donatorId}/{campaignId}/{userId}")
-    public ResponseEntity<?> createDonation(@PathVariable("userId") Long userId,
-                                            @PathVariable("donatorId") Long donatorId,
-                                            @PathVariable("campaignId") Long campaignId,
-                                            @RequestBody Donation donation) {
-        try {
-            Donation don = donationService.createDonation(userId, donatorId, campaignId, donation);
-            if (don != null) {
-                return ResponseEntity.ok("Donation created successfully!");
-            }
+    public ResponseEntity<?> createDonation(@PathVariable("userId") Long userId, @PathVariable("donatorId") Long donatorId, @PathVariable("campaignId") Long campaignId, @RequestBody Donation donation) {
+        ResponseEntity<?> don = donationService.createDonation(userId, donatorId, campaignId, donation);
+        if (don.getStatusCode() == HttpStatus.OK) {
+            return ResponseEntity.ok("Donation created successfully!");
+        } else {
             return ResponseEntity.ok("Donation has not been created!");
-        } catch (UserPermissionException | DonationRequirementsException | DonationException exception) {
-            return ResponseEntity.ok(exception.getMessage());
         }
-
     }
 
     @PutMapping("/{donationId}/{userId}")
-    public ResponseEntity<?> updateDonation(@PathVariable("userId") Long userId,
-                                            @PathVariable("donationId") Long donationId,
-                                            @RequestBody Donation newDonation) throws
-            DonationRequirementsException, DonationIdException, UserPermissionException, DonationNotFoundException, DonationApprovedException {
-
-        try {
-            Donation don = donationService.updateDonation(userId, donationId, newDonation);
-            if (don != null) {
-                return ResponseEntity.ok("Donation updated successfully!");
-            }
+    public ResponseEntity<?> updateDonation(@PathVariable("userId") Long userId, @PathVariable("donationId") Long donationId, @RequestBody Donation newDonation) {
+        ResponseEntity<?> don = donationService.updateDonation(userId, donationId, newDonation);
+        if (don.getStatusCode() == HttpStatus.OK) {
+            return ResponseEntity.ok("Donation updated successfully!");
+        } else {
             return ResponseEntity.ok("Donation has not been updated!");
-        } catch (DonationRequirementsException
-                 | DonationIdException
-                 | UserPermissionException
-                 | DonationNotFoundException
-                 | DonationApprovedException exception) {
-            return ResponseEntity.ok(exception.getMessage());
         }
-
     }
 
     @DeleteMapping("/{donationId}/{userId}")
-    public ResponseEntity<?> deleteDonationById(@PathVariable("userId") Long userId,
-                                                @PathVariable("donationId") Long donationId) {
-
-        try {
-            Donation donation = donationService.getDonationById(donationId);
-            if (donation != null) {
-                if (!donation.isApproved()) {
-                    donationService.deleteDonationById(userId, donationId);
-                    return ResponseEntity.ok("Donation has been deleted!");
-                }
+    public ResponseEntity<?> deleteDonationById(@PathVariable("userId") Long userId, @PathVariable("donationId") Long donationId) throws DonationNotFoundException {
+        Optional<Donation> donation = (Optional<Donation>) donationService.getDonationById(donationId).getBody();
+        //ResponseEntity<?> donation = donationService.getDonationById(donationId);
+        if (donation.isPresent()) {
+            if (!donation.get().isApproved()) {
+                donationService.deleteDonationById(userId, donationId);
+                return ResponseEntity.ok("Donation has been successfully deleted!");
+            } else {
                 return ResponseEntity.ok("Can't delete a donation which has been approved!");
             }
+        } else {
             return ResponseEntity.ok("Donation with given id does not exist!");
-        } catch (DonationIdException
-                 | DonationNotFoundException
-                 | DonationApprovedException
-                 | UserPermissionException exception) {
-            return ResponseEntity.ok(exception.getMessage());
         }
-
     }
-
 }
