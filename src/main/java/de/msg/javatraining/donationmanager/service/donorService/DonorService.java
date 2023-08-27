02e21944cace2f89b1fe20cccabcb2f84be33451
SergiuSavu <139860207+SergiuSavu@users.noreply.github.com>
2023-08-27 18:36:00 +0300

    Revert "Revert "Donation & Donor""

--- src/main/java/de/msg/javatraining/donationmanager/service/donorService/DonorService.java
+++ src/main/java/de/msg/javatraining/donationmanager/service/donorService/DonorService.java
@@ -54,66 +54,15 @@ public class DonorService {
         return donorRepository.findAll();
     }
 
-//    public Optional<Donator> getDonatorById(Long id) {
-//        return Optional.ofNullable(donatorRepository.findById(id)
-//                .orElseThrow(() -> new IllegalStateException("Donator with id: " + id + " does not exist.")));
-//    }
-
     public List<Donor> getDonatorsByCampaignId(Long id){
         return donorRepository.findDonatorsByCampaignId(id);
     }
-
-
-    // Asta este buna
-//    public ResponseEntity<?> getDonatorById(Long id) {
-//        Donator donator;
-//        try {
-//            if (donatorRepository.findById(id).isEmpty()) {
-//                throw new DonatorNotFoundException();
-//            }
-//            donator = donatorRepository.findById(id).get();
-//        } catch (DonatorNotFoundException exception) {
-//            return ResponseEntity.ok(exception.getMessage());
-//        }
-//        return ResponseEntity.ok(donator);
-//    }
 
     public Donor getDonatorById(Long id) throws DonatorNotFoundException {
         Donor donor = donorRepository.findById(id)
                 .orElseThrow(DonatorNotFoundException::new);
         return donor;
     }
-
-
-//    public Donator createDonator(Long userId, Donator donator) {
-//        if (checkDonatorRequirements(donator)) {
-//            if (checkUserPermission(userId, permission)) {
-//                donatorRepository.save(donator);
-//                return donator;
-//            } else {
-//                throw new IllegalStateException("User does not have the required permission!");
-//            }
-//        } else {
-//            throw new IllegalStateException("Donator requirements not met!");
-//        }
-//    }
-
-//    public ResponseEntity<?> createDonator(Long userId, Donator donator) {
-//        try {
-//            if (checkDonatorRequirements(donator)) {
-//                if (checkUserPermission(userId, permission)) {
-//                    donatorRepository.save(donator);
-//                    return ResponseEntity.ok(donator);
-//                } else {
-//                    throw new UserPermissionException();
-//                }
-//            } else {
-//                throw new DonatorRequirementsException();
-//            }
-//        } catch (UserPermissionException | DonatorRequirementsException exception) {
-//            return ResponseEntity.ok(exception.getMessage());
-//        }
-//    }
 
     public Donor createDonator(Long userId, Donor donor) throws
             UserPermissionException,
@@ -133,45 +82,6 @@ public class DonorService {
             throw new DonatorRequirementsException();
         }
     }
-
-
-//    public Donator deleteDonatorById(Long userId, Long donatorId) throws EntityNotFoundException {
-//        if (donatorId == null) {
-//            throw new IllegalArgumentException("DonatorId cannot be null.");
-//        }
-//        if (checkUserPermission(userId, permission)) {
-//            Donator donator = donatorRepository.findById(donatorId)
-//                    .orElseThrow(() -> new EntityNotFoundException("Donator with ID: " + donatorId + " not found."));
-//            donatorRepository.deleteDonatorById(donatorId);
-//            return donator;
-//        }
-//        else {
-//            throw new IllegalArgumentException("User does not have permission to delete a donator!");
-//        }
-//    }
-
-//    public ResponseEntity<?> deleteDonatorById(Long userId, Long donatorId) {
-//        try {
-//            if (donatorId == null) {
-//                throw new DonatorIdException();
-//            }
-//            Optional<Donator> donator = donatorRepository.findById(donatorId);
-//            if (donator.isPresent()) {
-//                if (checkUserPermission(userId, permission)) {
-//                    donatorRepository.deleteById(donatorId);
-//                    return ResponseEntity.ok(donator);
-//                } else {
-//                    throw new UserPermissionException();
-//                }
-//            } else {
-//                throw new DonatorNotFoundException();
-//            }
-//        } catch (DonatorIdException
-//                 | UserPermissionException
-//                 | DonatorNotFoundException exception) {
-//            return ResponseEntity.ok(exception.getMessage());
-//        }
-//    }
 
     public Donor deleteDonatorById(Long userId, Long donatorId) throws
             DonatorIdException,
@@ -198,37 +108,6 @@ public class DonorService {
             throw new UserPermissionException();
         }
     }
-
-
-//    public Donator updateDonator(Long userId, Long donatorId, Donator updatedDonator) {
-//        if (donatorId == null) {
-//            throw new IllegalArgumentException("DonatorId cannot be null.");
-//        }
-//        if (checkUserPermission(userId, permission)) {
-//            Donator donator = donatorRepository.findById(donatorId)
-//                    .orElseThrow(() -> new IllegalStateException(
-//                            "Donator with id: " + donatorId + " does not exist!"
-//                    ));
-//
-//            if (updatedDonator.getAdditionalName() != null) {
-//                donator.setAdditionalName(updatedDonator.getAdditionalName());
-//            }
-//            if (updatedDonator.getFirstName() != null) {
-//                donator.setFirstName(updatedDonator.getFirstName());
-//            }
-//            if (updatedDonator.getLastName() != null) {
-//                donator.setLastName(updatedDonator.getLastName());
-//            }
-//            if (updatedDonator.getMaidenName() != null) {
-//                donator.setMaidenName(updatedDonator.getMaidenName());
-//            }
-//            donatorRepository.save(donator);
-//            return donator;
-//        } else {
-//            throw new IllegalArgumentException("No permission to modify a donator");
-//        }
-//    }
-
 
     public Donor updateDonator(Long userId, Long donatorId, Donor updatedDonor) throws
             DonatorIdException,
@@ -271,43 +150,4 @@ public class DonorService {
         return donor;
     }
 
-//    public ResponseEntity<?> updateDonator(Long userId, Long donatorId, Donator updatedDonator) {
-//        try {
-//            if (donatorId == null) {
-//                throw new DonatorIdException();
-//            }
-//            if (checkDonatorRequirements(updatedDonator)) {
-//                if (checkUserPermission(userId, permission)) {
-//                    Optional<Donator> donator = donatorRepository.findById(donatorId);
-//                    if (donator.isPresent()) {
-//                        if (updatedDonator.getAdditionalName() != null) {
-//                            donator.get().setAdditionalName(updatedDonator.getAdditionalName());
-//                        }
-//                        if (updatedDonator.getFirstName() != null) {
-//                            donator.get().setFirstName(updatedDonator.getFirstName());
-//                        }
-//                        if (updatedDonator.getLastName() != null) {
-//                            donator.get().setLastName(updatedDonator.getLastName());
-//                        }
-//                        if (updatedDonator.getMaidenName() != null) {
-//                            donator.get().setMaidenName(updatedDonator.getMaidenName());
-//                        }
-//                        donatorRepository.save(donator.get());
-//                        return ResponseEntity.ok(donator);
-//                    } else {
-//                        throw new DonatorNotFoundException();
-//                    }
-//                } else {
-//                    throw new UserPermissionException();
-//                }
-//            } else {
-//                throw new DonatorRequirementsException();
-//            }
-//        } catch (DonatorIdException
-//                 | DonatorNotFoundException
-//                 | UserPermissionException
-//                 | DonatorRequirementsException exception) {
-//            return ResponseEntity.ok(exception.getMessage());
-//        }
-//    }
 }
